e04deee9693c1883c376997ec406b5a8eb402d0b
Juan Trilnardo Defrin <103166887+juantdefrin94@users.noreply.github.com>
2023-01-30 13:29:14 +0700

    Finishing Adapter Design Pattern

--- src/adapter/PaymentAdapter.java
+++ src/adapter/PaymentAdapter.java
@@ -1,9 +1,36 @@
 package adapter;
 
-public class PaymentAdapter {
+import models.AyoPay;
+import models.SophiPay;
+import models.User;
 
-	public PaymentAdapter() {
-		// TODO Auto-generated constructor stub
+public class PaymentAdapter implements IPaymentMethod {
+
+	private AyoPay ayoPay = null;
+	private SophiPay sophiPay = null;
+	private String paymentName = "";
+	
+	public PaymentAdapter(String paymentName, User currUser) {
+		this.paymentName = paymentName;
+		if(paymentName.equals("AyoPay")) {
+			ayoPay =  (AyoPay) currUser.getAyoPay();
+		}else if(paymentName.equals("SophiPay")) {
+			sophiPay = (SophiPay) currUser.getSophiPay();
+		}
+	}
+
+	@Override
+	public void payingBill(int bill) {
+		double disc = 0;
+		System.out.println("Total Bill : " + bill);
+		if(ayoPay != null) {
+			disc = ayoPay.getDisc();
+		}else if(sophiPay != null) {
+			disc = sophiPay.getDisc();
+		}
+		System.out.println("Discount : " + disc + "%");
+		System.out.println("Payment Method : " + paymentName);
+		System.out.println("Total Bill after Discount : " + ((double) bill - (double) bill * disc));
 	}
 
 }
